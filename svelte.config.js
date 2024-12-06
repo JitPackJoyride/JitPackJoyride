a5b2c3413bd79540a141cdcb316b84f69a5a3ee2
JitPackJoyride <ajitkrishna99@gmail.com>
2024-12-06 18:59:59 +0800

    feat: new sveltekit app

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,21 @@
-import { vitePreprocess } from '@astrojs/svelte';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-	preprocess: vitePreprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), mdsvex()],
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	},
+
+	extensions: ['.svelte', '.svx']
 };
+
+export default config;
